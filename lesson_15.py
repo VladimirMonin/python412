55bf0b671f83c70a468d098f61c9b2722ecb9af7
Vladimir <vladimir.monin2016@yandex.ru>
2024-12-03 21:42:39 +0500

    lesson_15: несколько return и функция работающая с коллекцией

--- lesson_15.py
+++ lesson_15.py
@@ -39,34 +39,34 @@ Python функции. Аргументы и модули. Библиотеки 
 
 # Позиционные VS именованные аргументы
 
-def get_message(name, message, age=18):
-    # Проверка на типы данных
-    if not isinstance(name, str):
-        raise TypeError('Имя должно быть строкой')
-    if not isinstance(message, str):
-        raise TypeError('Сообщение должно быть строкой')
-    if not isinstance(age, int):
-        raise TypeError('Возраст должен быть числом')
+# def get_message(name, message, age=18):
+#     # Проверка на типы данных
+#     if not isinstance(name, str):
+#         raise TypeError('Имя должно быть строкой')
+#     if not isinstance(message, str):
+#         raise TypeError('Сообщение должно быть строкой')
+#     if not isinstance(age, int):
+#         raise TypeError('Возраст должен быть числом')
 
-    if age < 18:
-        message += ' (несовершеннолетний)'
-    else:
-        message += ' (совершеннолетний)'
+#     if age < 18:
+#         message += ' (несовершеннолетний)'
+#     else:
+#         message += ' (совершеннолетний)'
     
     
-    name = name.capitalize()
-    message = message.lower()
-    return f'Твоё имя:{name}\nСообщение для тебя:{message}!'
+#     name = name.capitalize()
+#     message = message.lower()
+#     return f'Твоё имя:{name}\nСообщение для тебя:{message}!'
 
-print(get_message('Вася', 'привет!'))
-print(get_message('Таня', 'привет!'))
-print(get_message('Боб', 'Пока!', 16))
+# print(get_message('Вася', 'привет!'))
+# print(get_message('Таня', 'привет!'))
+# print(get_message('Боб', 'Пока!', 16))
 # print(get_message('Пока')) # TypeError: get_message() missing 1 required positional argument: 'message'
 # print(get_message('Пока', 'Боб!', 'Таня')) # TypeError: get_message() takes 2 positional arguments but 3 were given
 
-result = get_message('Вася', 'привет!', 16)
-result = get_message(age=16, name='Вася', message='привет!')
-result = get_message(message='привет!', name='Вася')
+# result = get_message('Вася', 'привет!', 16)
+# result = get_message(age=16, name='Вася', message='привет!')
+# result = get_message(message='привет!', name='Вася')
 
 
 #PRACTICE - функция проверки на палиндром одного слова
@@ -100,10 +100,52 @@ list[str]
 dict[str, int]
 """
 
-user_word = input('Введите слово: ')
+# user_word = input('Введите слово: ')
 
-if is_palindrome(user_word):
-    print('Это палиндром')
-else:
-    print('Это не палиндром')
+# if is_palindrome(user_word):
+#     print('Это палиндром')
+# else:
+#     print('Это не палиндром')
 
+
+def check_palindrome(word:str) -> str:
+    """
+    Функция проверки на палиндром от Спартака.
+    Возвращает строку с отчётом
+    """
+    raw_word = word.lower().replace(' ', '')
+    if raw_word == raw_word[::-1]:
+        return f'{word} - это палиндром'
+    else:
+        return f'{word} - не палиндром'
+
+
+# print(check_palindrome(input('Введите слово: ')))
+
+
+words = ["хлеб", "кот", "дед", "мама"]
+
+# def check_many_palindromes(words:list[str]) -> str:
+#     for word in words:
+#         if is_palindrome(word):
+#             return f'{word} - это палиндром'
+    
+#     return 'Нет палиндромов'
+   
+        
+# # Протестируем как это будет работать
+# print(check_many_palindromes(words))
+
+
+def check_many_palindromes(words:list[str]) -> dict[str, bool]:
+    result = {}
+    for word in words:
+        if is_palindrome(word):
+            result[word] = True
+        else:
+            result[word] = False
+    
+    return result
+
+# Тест
+print(check_many_palindromes(words))
