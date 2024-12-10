1f9e6a2cb8de0d314a4f5c7fc6acac8965939bd3
Vladimir <vladimir.monin2016@yandex.ru>
2024-12-10 16:12:08 +0500

    lesson_16: написали функции и разложили по модулям

--- lesson_16/image_compressor.py
+++ lesson_16/image_compressor.py
@@ -3,31 +3,91 @@
 Работает на pillow и сжимает то что дают на входе в webp
 pip install pillow
 """
-# Импорт 
-from PIL import Image # pip install pillow
-import os # встроенный в пайтон модуль для работы с ОС
 
-ALLOWED_EXTENSIONS = ['jpg', 'jpeg', 'png']
+# Импорт
+from PIL import Image  # pip install pillow
+import os  # встроенный в пайтон модуль для работы с ОС
+
+ALLOWED_EXTENSIONS = ["jpg", "jpeg", "png"]
 COMPRESS_PERCENT = 50
 
-# Получаем путь к файлу
-user_file = input('Введите путь к файлу: ').strip().strip('"').strip("'")
 
-# Получаем абсолютный путь к файлу с помощью модуля os
-file_path = os.path.abspath(user_file)
+def get_path_to_file():
+    """
+    Функция для получения пути к файлу
+    """
+    user_file = input("Введите путь к файлу: ").strip().strip('"').strip("'")
+    os_path = os.path.abspath(user_file)
+    return os_path
 
-# Получаем рассширение файла
-file_extension = file_path.split('.')[-1]
 
-# Проверяем расширение файла
-if file_extension not in ALLOWED_EXTENSIONS:
-    raise ValueError('Недопустимое расширение файла')
+def get_file_extension(file_path):
+    """
+    Функция для получения расширения файла
+    """
+    file_extension = file_path.split(".")[-1]
+    return file_extension
 
-# Открываем файл
-image = Image.open(file_path)
 
-# Определяем новое имя файла
-new_file_name = f'{file_path.split(".")[0]}_compressed.webp'
+def get_file_name(file_path):
+    """
+    Функция для получения названия файла
+    """
+    file_name = os.path.basename(file_path)
+    no_extension_name = file_name.split(".")[0]
+    return no_extension_name
 
-# Сжимаем изображение и сохраняем
-image.save(new_file_name, 'webp', quality=COMPRESS_PERCENT)
+
+def compress_image(file_path, compress_percent):
+    """
+    Функция для сжатия изображения
+    """
+    image = Image.open(file_path)
+    # Получаем директорию исходного файла
+    source_dir = os.path.dirname(file_path)
+    # Формируем полный путь для нового файла
+    new_file_name = os.path.join(
+        source_dir, f"{get_file_name(file_path)}_compressed.webp"
+    )
+    image.save(new_file_name, "webp", quality=compress_percent)
+
+
+def compress_images_in_dir(dir_path):
+    """
+    Функция для сжатия изображений в директории
+    """
+    # os.listdir(dir_path) - возвращает список файлов в директории
+    for file_name in os.listdir(dir_path):
+        # os.path.join(dir_path, file_name) - возвращает полный путь к файлу
+        file_path = os.path.join(dir_path, file_name)
+        # os.path.isfile(file_path) - проверяет является ли объект файлом
+        if os.path.isfile(file_path):
+            file_extension = get_file_extension(file_path)
+            if file_extension in ALLOWED_EXTENSIONS:
+                compress_image(file_path, COMPRESS_PERCENT)
+
+
+def main():
+    print("Выберите действие:")
+    print("1. Сжать изображение")
+    print("2. Сжать изображения в директории")
+    user_choice = input("Введите номер действия: ")
+
+    if user_choice == "1":
+        file_path = get_path_to_file()
+        file_extension = get_file_extension(file_path)
+
+        if file_extension not in ALLOWED_EXTENSIONS:
+            raise ValueError("Недопустимое расширение файла")
+        compress_image(file_path, COMPRESS_PERCENT)
+
+    elif user_choice == "2":
+        dir_path = get_path_to_file()
+        compress_images_in_dir(dir_path)
+
+    else:
+        print("Неизвестное действие")
+
+
+if __name__ == "__main__":
+    main()
---
+++ lesson_16/main.py
@@ -0,0 +1,8 @@
+# from image_compressor import *
+# from lesson_16 import image_compressor
+# image_compressor.compress_image()
+
+from image_compressor import main
+
+if __name__ == "__main__":
+    main()
