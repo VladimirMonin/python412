04712332c48e3a93f9870cb45e8683c09ba53ec4
Vladimir <vladimir.monin2016@yandex.ru>
2024-12-19 21:56:18 +0500

    lesson_20: знакомство с генераторами

--- lesson_20/lesson_20.py
+++ lesson_20/lesson_20.py
@@ -34,8 +34,8 @@ all_list = [True, True, True]
 any_list = [True, False, False]
 
 # Как будет работать all и any
-print(all(all_list)) # True
-print(all(any_list)) # False
+print(all(all_list))  # True
+print(all(any_list))  # False
 
 print(any(all_list))  # True
 print(any(any_list))  # True
@@ -44,17 +44,90 @@ print(any(any_list))  # True
 nums_list = ["1", "2", "3", "4", "5"]
 
 # Проверим через all что все элементы проходят isdigit
-all_nums = all(map(str.isdigit, nums_list)) # True
+all_nums = all(map(str.isdigit, nums_list))  # True
 
 # Создание генераторного выражения
 all_nums = all(num.isdigit() for num in nums_list)
 
 
+from math import pi
 from cities import cities_list
 
 # Проверим, что в наборе есть город с населением более 10000000
 
 result = any(map(lambda city: city["population"] > 10000000, cities_list))
-print(result) # True
+print(result)  # True
 
-# 
+# Генераторы - функции которые возвращают по одному элементу
+# Генератор пирожков
+
+
+def pie_generator(n):
+    for i in range(n):
+        yield f"Пирожок {i}"
+
+
+# Объект - генератор
+five_pies = pie_generator(5)
+print(type(five_pies))  # <class 'generator'>
+print(five_pies)  # <generator object pie_generator at 0x000001E714C7A9B0>
+
+print(next(five_pies))  # Пирожок 0
+print(next(five_pies))  # Пирожок 1
+print(next(five_pies))  # Пирожок 2
+# print(next(five_pies))  # Пирожок 3
+# print(next(five_pies))  # Пирожок 4
+# print(next(five_pies))  # Пирожок 5
+# print(next(five_pies))  # StopIteration Exception
+
+for pie in five_pies:
+    print(f"Пирожок из цикла: {pie}")
+
+ten_pies = pie_generator(10)
+
+pies_list = list(ten_pies)
+print(pies_list) 
+# print(next(ten_pies))  # StopIteration Exception
+
+
+# Функция, создающая огромный список в памяти
+def get_huge_list(n):
+    result = []
+    for i in range(n):
+        result.append(i ** 2)
+    return result
+
+# Генератор, выдающий значения по одному
+def get_huge_generator(n):
+    for i in range(n):
+        yield i ** 2
+
+# Демонстрация разницы
+# Функция попытается создать огромный список в памяти
+# print("Начинаем работу с функцией...")
+# huge_list = get_huge_list(100_000_000)  # Это займет много памяти и времени
+
+# Генератор будет выдавать значения по одному
+# print("Начинаем работу с генератором...")
+# for i, value in enumerate(get_huge_generator(100_000_000)):
+#     if i % 1_000_000 == 0:
+#         print(f"Сгенерировано {i:,} значений. Текущее значение: {value}")
+
+
+
+
+
+def cities_by_population(min_population: int):
+    for city in cities_list:
+        if city["population"] > min_population:
+            yield f'Город: {city["name"]}, население: {city["population"]}'
+
+
+user_num = int(input("Введите минимальное население: "))
+
+for city in cities_by_population(user_num):
+    print(city)
+    user_answer = input("Хотите продолжить? (y/n): ")
+    if user_answer.lower() == "n":
+        print("До свидания!")
+        break
