6987b7cb373a943984b884c2dfca1d7426119bd1
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-09 23:27:18 +0500

    lesson_23 асинхронная работа с погодным API и aiohttp

--- lesson_23.py
+++ lesson_23.py
@@ -10,46 +10,46 @@ Lesson 23
 - 
 """
 
-# Импортируем модуль для работы с асинхронностью
 import asyncio
-# Импортируем time.perf_counter()
 import time
+# pip install aiohttp
+import aiohttp
 
 
-# Напишем функцию, которая будет выполняться асинхронно
-# принемает время выполнения в секундах и название задачи
+# Функция для асинхронного GET запроса, принемает URL и возвращает ответ
+async def get_url(url: str):
+    session = aiohttp.ClientSession()
+    response = await session.get(url)
+    result = await response.json()
+    await session.close()
+    await asyncio.sleep(0.2) # Асинхронный сон в 0.2 секунды. Выходит, что не более 5 запросов в секунду.
+    return result
 
-async def task(time: int, name: str) -> str:
-    # Выведем сообщение
-    print(f"Начало задачи {name}")
-    # await - позволит перейти к другим задачам
-    # asyncio.sleep - имитация ожидания ввода - вывода
-    await asyncio.sleep(time)
-    # Выведем сообщение
-    print(f"Задача {name} выполнена")
-    return f'{name}: сделано!'
+# Так как мы будем делать запросы к погодному АПИ (Сразу по разным городам)
+# Нам нужна функция которая на вход возьмет город и отдаст URL для запроса
+# url = r'https://api.openweathermap.org/data/2.5/weather?q=Москва&appid=23496c2a58b99648af590ee8a29c5348&units=metric&lang=ru'
 
+def get_url_by_city(city: str) -> str:
+    url = fr'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=23496c2a58b99648af590ee8a29c5348&units=metric&lang=ru'
+    return url
 
-# ЗАВТРАК!
+# Москва Питер Екатеринбург Краснодар Омск
+
 async def main():
-    # Включаем чайник IO bound - оно будет идти конкурентно
-    pot = asyncio.create_task(task(5, "Чайник"))
+    # print(f'Москва', await get_url(get_url_by_city('Москва')))
+    # print(f'Санкт-Петербург', await get_url(get_url_by_city('Санкт-Петербург')))
+    # print(f'Екатеринбург', await get_url(get_url_by_city('Екатеринбург')))
+    # print(f'Краснодар', await get_url(get_url_by_city('Краснодар')))
+    # print(f'Омск', await get_url(get_url_by_city('Омск')))
 
-    # Достаем колбасу и сыр из холодильника и нарезаем хлеб
-    # CPU bound - оно будет идти последовательно
-    cheese = await task(2, "Сыр и колбаса")
-    bread = await task(2, "Хлеб")
+    # cities = ['Москва', 'Санкт-Петербург', 'Екатеринбург', 'Краснодар', 'Омск']
+    # results = await asyncio.gather(*[get_url(get_url_by_city(city)) for city in cities])
 
-    # Дождемся чайника
-    hot_water = await pot
-
-    # Заварить чай
-    # CPU bound - оно будет идти последовательно
-    black_tea = await task(3, "Чай")
-
-
-
-    
+    # Используем geather
+    cities = ['Москва', 'Санкт-Петербург', 'Екатеринбург', 'Краснодар', 'Омск']
+    tasks = [asyncio.create_task(get_url(get_url_by_city(city))) for city in cities]
+    results = await asyncio.gather(*tasks)
+    print(results)
 
 # Засекаем старт
 start_time = time.perf_counter()
