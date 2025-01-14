6e1628375a8f92048cd6416475072daf55e63792
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-14 23:33:50 +0500

    lesson_24: методы экземпляра и атрибуты экземпляра

--- lesson_24.py
+++ lesson_24.py
@@ -13,16 +13,40 @@ Lesson 24
 UpperCamelCase - для классов
 Используем существительные и прилагательные
 """
+"""
+Dunder underscope методы, дандер методы, или магические методы
+__init__ - инициализатор
+"""
+
+class SimpleCar:
+    mark = "Lada"
+
+simple_car_1 = SimpleCar()
+simple_car_2 = SimpleCar()
+
+simple_car_1.mark = "BMW"
+simple_car_1.color = "red"
+
+print(simple_car_1.mark)
+print(simple_car_1.color)
 
 class Car:
-    mark = "Ёмобиль" # Поле или атрибут класса
+    def __init__(self, mark: str, color: str):
+        self.mark = mark
+        self.color = color
+        print(self)
 
-car = Car()
-car_1 = Car()
-print(car)
-print(type(car))
+    def drive(self):
+        print(f"Машина {self.mark} едет")
 
-print(car_1)
 
-print(car.mark)
-print(car_1.mark)
+# car1 = Car() # TypeError: Car.__init__() missing 1 required positional argument: 'mark'
+
+car1 = Car("BMW", "чёрный")
+car2 = Car("Audi", "зелёная")
+
+print(car1.mark)
+print(car2.mark)
+
+car1.drive()
+car2.drive()
