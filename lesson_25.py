b38f132bfb5c8e98eac8e7cba3245f35f67ce1da
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-16 22:49:27 +0500

    lesson_25: методы класса и методы экземпляра

--- lesson_25.py
+++ lesson_25.py
@@ -7,6 +7,8 @@ Lesson 25
 - Методы и атрибуты экземпляров
 - self
 - __call__
+- @classmethod
+- @staticmethod
 """
 
 
@@ -15,9 +17,27 @@ class JpegImage:
     Класс - муляж для демонстрации полиморфизма на примере работы с разными типами изображений
     """
 
+    extensions = ["jpg", "jpeg"]
+
     def __init__(self, file_path: str):
         self.file_path = file_path
 
+    @classmethod
+    def get_available_extensions(cls):
+        """
+        Метод возвращает список доступных расширений
+        """
+        return cls.extensions
+    
+    @classmethod
+    def is_valid_extension(cls, extension: str) -> bool:
+        """
+        Метод проверяет расширение на валидность
+        :param extension: Расширение файла
+        :return: True/
+        """
+        return extension in cls.extensions
+    
     def open(self):
         """
         Метод имитирующий открытие файла
@@ -32,38 +52,15 @@ class JpegImage:
         """
         print(f"Обрезаем файл {self.file_path} до ширины: {width} и высоты {heigth}")
 
-
-class PngImage:
-    """
-    Класс - муляж для демонстрации полиморфизма на примере работы с разными типами изображений
-    """
-
-    def __init__(self, file_path: str):
-        self.file_path = file_path
-
-    def open(self):
+    @staticmethod
+    def image_analys(file_path: str):
         """
-        Метод имитирующий открытие файла
+        Анализ файла
         """
-        print(f"Открываем {self.file_path}")
+        print(f'{file_path} - это файл')
 
-    def crop(self, heigth, width):
-        """
-        Метод имитирующий обрезку файла
-        :param heigth: Выоста
-        :param width: ширина
-        """
-        print(f"Обрезаем файл {self.file_path} до ширины: {width} и высоты {heigth}")
+    
 
 
-image_jpeg_1 = JpegImage("image_1.jpeg")
-image_jpeg_2 = JpegImage("image_2.jpeg")
-image_png_1 = PngImage("image_1.png")
-image_png_2 = PngImage("image_2.png")
-
-
-images = [image_jpeg_1, image_jpeg_2, image_png_1, image_png_2]
-
-for image in images:
-    image.open()
-    image.crop(100, 100)
+image = JpegImage("image.jpg")
+image.image_analys("банан.jpg")
