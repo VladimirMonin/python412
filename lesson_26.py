4550ed9517fac9cd7985efab17621e154e73ed1d
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-21 21:36:07 +0500

    lesson_26: работа с приватными методами и атрибутами на примере индексации ЗП

--- lesson_26.py
+++ lesson_26.py
@@ -12,29 +12,33 @@ __ - приватные атрибуты и методы
 """
 
 class Employee:
-    def __init__(self, name: str, age: int, salary: int):
+    def __init__(self, name: str, age: int, salary: int|float):
         self.name = name
         self.__age = age
         self._salary = salary
+        self.__experience: int = 0
+        self.__salary_index: int = 10
 
     def _get_dollars_salary(self, cource):
         result = round(self._salary / cource, 2)
         return result
     
+    def __set_salary(self, value: int|float):
+        self._salary = value
+
+    def add_experience(self):
+        self.__experience += 1
+        new_salary = self._salary + (self._salary * self.__salary_index / 100)
+        self.__set_salary(new_salary)
+
+    
     def __str__(self):
-        return f"Имя: {self.name}\nВозраст: {self.__age}\nЗарплата: {self._salary}"
+        return f"Имя: {self.name}\nВозраст: {self.__age}\nЗарплата: {self._salary}. Выслуга: {self.__experience} лет"
 
 
 employee = Employee("Спартак", 30, 300000)
-print(employee.name)
-# print(employee.age) # AttributeError: 'Employee' object has no attribute 'age'
-# Как добыть всё же приватный атрибут возраст? Пайтон искажает к ним путь.
-print(employee._Employee__age) # переменная._НазваниеКласса__название_атрибута
-employee.age = 25
 print(employee)
-print(employee.age)
-print(employee._salary)
-print(employee._get_dollars_salary(105))
-
-# Вызовем dict на объекте
-print(employee.__dict__)
+employee.add_experience()
+print(employee)
+employee.add_experience()
+print(employee)
