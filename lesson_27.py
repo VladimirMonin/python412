d27322259a35179886eb751a269ab0df9ae0557a
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-23 23:09:14 +0500

    lesson_27: Animal и Dog базовый вариант наследования

--- lesson_27.py
+++ lesson_27.py
@@ -10,60 +10,23 @@ Lesson 27
 Employee и  Organisaton как пример взаимодействия классов
 Класс ManagerEmployee и потребность в наследовании при незначительных вариациях логики и атрибутов
 Базовый пример наследования в коде. DRY - избегаем повторения кода (а так же сложных аннотаций типов)
+Animal и Dog базовый вариант наследования
+self.__class__.__name__
 """
-class Employee:
-    def __init__(self, name: str, age: int, salary: int | float, position: str):
-        self.name = name
-        self.age = age
-        self.salary = salary
-        self.position = position
 
-    def __str__(self):
-        return f"Сотрудник: {self.name}, возраст: {self.age}, зарплата: {self.salary}, должность: {self.position}"
-
-    def drink_coffee(self):
-        print(f"{self.name} в должности {self.position} выпил кофе")
-
-    def work(self):
-        print(f"{self.name} поработал")
+class Animal:
+    def voise(self):
+        # self. - экземпляр
+        # __class__ обращение к своему классу
+        # __name__ получение имени класса в виде строки
+        print(f"{self.__class__.__name__} подаёт голос")
 
 
-class ManagerEmployee(Employee):
-        def hit_coworker(self, coworker):
-            print(f"{self.name} ударил {coworker.name}")
-
-        def work(self):
-            print(f"{self.name} мотивирует коллег")
+class Dog(Animal):
+    pass
 
 
-class Accountant(Employee):
-    def work(self):
-        print(f"{self.name} считает деньги")
-
-
-class Organisaton:
-    def __init__(self, name: str, employees: list[Employee]):
-        self.name = name
-        self.employees = employees
-
-    def add_employee(self, employee: Employee):
-        self.employees.append(employee)
-
-    def delete_employee(self, employee: Employee):
-        self.employees.remove(employee)
-
-
-# Тест
-organisation = Organisaton("ООО Рога и Копыта", [])
-# Создадим сотрудников
-base_employee = Employee("Андрей", 25, 100000, "Программист")
-manager_employee = ManagerEmployee("Владимир", 30, 150000, "Менеджер")
-accountant = Accountant("Алексей", 35, 120000, "Бухгалтер")
-
-# Добавим сотрудников в организацию
-organisation.add_employee(base_employee)
-organisation.add_employee(manager_employee)
-organisation.add_employee(accountant)
-
-# Вызов метода работа
-[employee.work() for employee in organisation.employees]
+dog = Dog()
+animal = Animal()
+dog.voise() # Dog подает голос
+animal.voise() # Animal подает голос
