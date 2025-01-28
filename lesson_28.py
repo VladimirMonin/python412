10731c381cf2b86915f6265146b4472d1340383f
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-28 23:27:05 +0500

    lesson_28: Передача **kwargs с любым порядком множественного наследования без ошибок в class Duck(Animal, Swim, Fly)

--- lesson_28.py
+++ lesson_28.py
@@ -7,42 +7,41 @@
 
 # Множественное наследование
 
-class Animal():
-    def __init__(self, name: str, age: int):
-        self.name = name
-        self.age = age
-
+class Animal:
+    def __init__(self, **kwargs):
+        self.name = kwargs.pop('name', None)  # pop удаляет использованные ключи
+        self.age = kwargs.pop('age', None)
+        super().__init__(**kwargs)
 
 class Swim:
-    def __init__(self, deep: int):
-        self.deep = deep
+    def __init__(self, **kwargs):
+        self.deep = kwargs.pop('deep', None)
+        super().__init__(**kwargs)
 
     def swim(self):
-        return f"Ныряет на глубину {self.deep} метров"
-    
-
+        print(f"{self.name} плавает и ныряет на глубину {self.deep} метров")
 
 class Fly:
-    def __init__(self, speed: int, height: int):
-        self.speed = speed
-        self.height = height
+    def __init__(self, **kwargs):
+        self.speed = kwargs.pop('speed', None)
+        self.height = kwargs.pop('height', None)
+        super().__init__(**kwargs)
 
     def fly(self):
-        return f"Летает со скоростью {self.speed} км/ч на высоту {self.height} метров"
-    
+        print(
+            f"{self.name} летит со скоростью {self.speed} км/ч на высоте {self.height} метров"
+        )
+
 
 class Duck(Animal, Swim, Fly):
-    def __init__(
-        self, name: str, age: int, deep: int, speed: int, height: int, color: str
-    ):
-        Animal.__init__(self, name, age)
-        Swim.__init__(self, deep)
-        Fly.__init__(self, speed, height)
-        self.color = color
+    def __init__(self, **kwargs):
+        self.color = kwargs.pop('color', None)
+        super().__init__(**kwargs)
+
+
 
 
 print(Duck.__mro__)
-duck = Duck("Дональд", 2, 5, 100, 1000, "белый")
-print(duck.swim())
-print(duck.fly())
+duck = Duck(name="Дональд", age=10, deep=10, speed=100, height=1000, color="белый")
+
 # (<class '__main__.Duck'>, <class '__main__.Animal'>, <class '__main__.Swim'>, <class '__main__.Fly'>, <class 'object'>)
