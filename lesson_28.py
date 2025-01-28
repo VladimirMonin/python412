a6bb466632ceb8dbd8756e16675def199b6063d1
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-28 21:35:09 +0500

    lesson_28: ABC и abstractmethod

--- lesson_28.py
+++ lesson_28.py
@@ -7,39 +7,43 @@ Phone (+ номер телефона)
 Laptop (+ размер экрана)
 Tablet (+ поддержка стилуса)
 """
+# Absctract Base Class
+from abc import ABC, abstractmethod
 
+# 1. Наследование от  ABC
+# 2. Декораторы абстрактных методов
 
-class Device:
-    avalible_brands = ["Apple", "Samsung", "Xiaomi", "Huawei", "Nokia"]
+class AbstractFile(ABC):
 
-    def __init__(self, brand: str, model: str):
-        self.brand = self.__validate_brand(brand)
-        self.model = model
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self, data: list[dict]):
+        pass
+
+    @abstractmethod
+    def append(self, data: list[dict]):
+        pass
 
     def __str__(self):
-        return f"Тип девайса:{self.__class__.__name__}\n{self.brand} {self.model}"
-    
-    def __validate_brand(self, brand):
-        if brand not in self.__class__.avalible_brands:
-            raise ValueError(f"Бренд {brand} не поддерживается")
-        return brand
-    
-    def turn_on(self):
-        return f"Включаем {self.brand} {self.model}"
-    
-    def turn_off(self):
-        return f"Выключаем {self.brand} {self.model}"
-    
-
-class Iphone(Device):
-    def __init__(self, brand: str, model: str, memory: int, color: str):
-        super().__init__(brand, model)
-        self.memory = memory
-        self.color = color
-
-    def turn_light(self):
-        return f"Включаем подсветку яблочка"
+        return f"{self.__class__.__name__} {self.file_path}"
 
 
-phone = Iphone("Apple", "Iphone 17 Pro Max", 2048, "Black")
-phone.turn_on()
+class TxtFile(AbstractFile):
+    
+    def read(self):
+        pass
+
+    def write(self):
+        pass
+
+    def append(self):
+        pass
+
+
+txt_file = TxtFile("file.txt")
