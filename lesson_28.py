1e1bdf3a76b7ed0cb7e5e4ac46e6a566de55d431
Vladimir <vladimir.monin2016@yandex.ru>
2025-01-28 21:21:55 +0500

    lesson_28: наследование с использованием своего и родительского инициализатора

--- lesson_28.py
+++ lesson_28.py
@@ -10,12 +10,19 @@ Tablet (+ поддержка стилуса)
 
 
 class Device:
+    avalible_brands = ["Apple", "Samsung", "Xiaomi", "Huawei", "Nokia"]
+
     def __init__(self, brand: str, model: str):
-        self.brand = brand
+        self.brand = self.__validate_brand(brand)
         self.model = model
 
     def __str__(self):
         return f"Тип девайса:{self.__class__.__name__}\n{self.brand} {self.model}"
+    
+    def __validate_brand(self, brand):
+        if brand not in self.__class__.avalible_brands:
+            raise ValueError(f"Бренд {brand} не поддерживается")
+        return brand
     
     def turn_on(self):
         return f"Включаем {self.brand} {self.model}"
@@ -25,10 +32,14 @@ class Device:
     
 
 class Iphone(Device):
-    def __init__(self, memory: int, color: str):
+    def __init__(self, brand: str, model: str, memory: int, color: str):
+        super().__init__(brand, model)
         self.memory = memory
         self.color = color
 
+    def turn_light(self):
+        return f"Включаем подсветку яблочка"
 
-phone = Iphone(128, "Black")
+
+phone = Iphone("Apple", "Iphone 17 Pro Max", 2048, "Black")
 phone.turn_on()
