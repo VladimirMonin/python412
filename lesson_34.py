e77efb2549539c6005f307c1425c549840334aa3
Vladimir <vladimir.monin2016@yandex.ru>
2025-02-18 21:53:27 +0500

    lesson_34: паттерн Состояние на простом примере

--- lesson_34.py
+++ lesson_34.py
@@ -13,97 +13,51 @@
 
 """
 
-# Паттерн Состояние на примере музыкального плеера.
+# Паттерн Состояние на примере нескольких разных действий на кухне
 
 from abc import ABC, abstractmethod
 
+# СОСТОЯНИЯ
 
-# Абстрактное состояние
-
-class PlayerState(ABC):
+class KitchenState(ABC):
     @abstractmethod
-    def play(self) -> "PlayerState":
+    def run(self):
         pass
 
-    @abstractmethod
-    def stop(self) -> "PlayerState":
-        pass
+class Cooking(KitchenState):
+    def run(self):
+        print("Приготовление блюда")
 
-    @abstractmethod
-    def pause(self) -> "PlayerState":
-        pass
+class Washing(KitchenState):
+    def run(self):
+        print("Мытье посуды")
 
+class LookingRefrigerator(KitchenState):
+    def run(self):
+        print("Задумчиво смотрим в холодильник")
 
-# Конкретные состояния
-class PlayState(PlayerState):
-    def play(self):
-        print(f'{self.__class__.__name__} - Проигрывание музыки')
-        return self
+# КОНТЕКСТ
 
-    def stop(self):
-        print(f'{self.__class__.__name__} - Остановка музыки. Переход в состояние StopState')
-        return StopState()
-
-    def pause(self):
-        print(f'{self.__class__.__name__} - Пауза. Переход в состояние PauseState')
-        return PauseState()
-
-
-class StopState(PlayerState):
-    def play(self):
-        print(f'{self.__class__.__name__} - Проигрывание музыки. Переход в состояние PlayState')
-        return PlayState()
-
-    def stop(self):
-        print(f'{self.__class__.__name__} - Музыка уже остановлена')
-        return self
-
-    def pause(self):
-        print(f'{self.__class__.__name__} - Музыка остановлена. Переход в состояние PauseState')
-        return StopState()
-    
-class PauseState(PlayerState):
-    def play(self):
-        print(f'{self.__class__.__name__} - Проигрывание музыки. Переход в состояние PlayState')
-        return PlayState()
-
-    def stop(self):
-        print(f'{self.__class__.__name__} - Остановка музыки. Переход в состояние StopState')
-        return StopState()
-
-    def pause(self):
-        print(f'{self.__class__.__name__} - Музыка на паузе')
-        return self
-
-
-# Контекст
-class Player:
+class Kitchen:
     def __init__(self):
-        self.state: PlayerState = StopState()
+        self.state: KitchenState = LookingRefrigerator()
 
-    def change_state(self, state: PlayerState):
+    def set_state(self, state: KitchenState):
+        if not isinstance(state, KitchenState):
+            raise TypeError("Передано неверное состояние")
         self.state = state
 
-    def play(self) -> None:
-        self.change_state(self.state.play())
+    def run(self):
+        self.state.run()
 
-    def stop(self) -> None:
-        self.change_state(self.state.stop())
-
-    def pause(self) -> None:
-        self.change_state(self.state.pause())
-
+# ИСПОЛЬЗОВАНИЕ
 
 if __name__ == "__main__":
-    player = Player() 
-    player.play() #  StopState - Проигрывание музыки. Переход в состояние PlayState
-    player.pause() #  PlayState - Пауза. Переход в состояние PauseState
-    player.play() # PauseState - Проигрывание музыки. Переход в состояние PlayState
-    player.stop() # PlayState - Остановка музыки. Переход в состояние StopState
-    player.stop() #  StopState - Музыка уже остановлена
-    player.pause() # StopState - Музыка остановлена. Переход в состояние PauseState
-    player.play() #  StopState - Проигрывание музыки. Переход в состояние PlayState
-    player.stop() # PlayState - Остановка музыки. Переход в состояние StopState
-    player.play() #  StopState - Проигрывание музыки. Переход в состояние PlayState
-    player.play() #  PlayState - Проигрывание музыки
-    player.stop() # PlayState - Остановка музыки. Переход в состояние StopState
+    kitchen = Kitchen()
+    kitchen.run() # Задумчиво смотрим в холодильник
+    kitchen.set_state(Cooking())
+    kitchen.run() # Приготовление блюда
+    kitchen.set_state(Washing())
+    kitchen.run() # Мытье посуды
+    kitchen.set_state(LookingRefrigerator())
+    kitchen.run() # Задумчиво смотрим в холодильник
