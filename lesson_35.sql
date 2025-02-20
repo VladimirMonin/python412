d654fb6e2fcc626baae428609e4da0c352ed1dd6
Vladimir <vladimir.monin2016@yandex.ru>
2025-02-20 23:18:54 +0500

    lesson_35: оператор IN

--- lesson_35.sql
+++ lesson_35.sql
@@ -7,6 +7,8 @@
 -- Использование операторов сравнения (=, <, >, BETWEEN) и логических операторов (AND, OR, NOT)
 -- DISTINCT - команда, которая позволяет выбрать уникальные значения
 -- ORDER BY - команда, которая позволяет сортировать данные
+-- LIKE - команда, которая позволяет искать строки, которые содержат определенный шаблон
+-- IN - команда, которая позволяет выбрать строки, которые содержат одно из значений из списка
 
 -- 1. ВЫБРАТЬ ВСЁ из таблицы MarvelCharacters
 SELECT *
@@ -62,3 +64,50 @@ WHERE SEX = "Genderfluid Characters"
     OR SEX = "Agender Characters" 
     AND appearances > 10
 ORDER BY SEX, appearances DESC
+
+-- 9 Получим уникальный год, сортируем и получаем первую десятку
+SELECT DISTINCT year
+FROM MarvelCharacters
+ORDER BY year
+LIMIT 10
+
+-- 10. Мы увидели что минимальный год 1939 и можем посмотреть персонажей появившихся в период 1940 - 1945
+SELECT name, year, appearances
+FROM MarvelCharacters
+WHERE year > 1939 
+AND year < 1946
+AND appearances > 20
+ORDER BY appearances DESC
+
+-- 11. Такой же, но с использованием BETWEEN
+SELECT name, year, appearances
+FROM MarvelCharacters
+WHERE year BETWEEN 1940 AND 1945
+AND appearances > 20
+ORDER BY appearances DESC
+
+
+-- 12. Поиск по шаблону
+-- LIKE - ищет "похожие" строки
+-- Шаблон - % - любое количество символов
+-- Шаблон - _ - один символ
+-- Шаблон - [abc] - один символ из набора ???
+-- Шаблон - [a-z] - один символ из диапазона ???
+
+--"Adolf%" - ищем строки начинающиеся с Adolf
+-- "%man%" - вхождение man в любом месте внутри строки
+SELECT name, year, appearances
+FROM MarvelCharacters
+WHERE name LIKE '% man %'
+OR name LIKE '%-man %'
+
+-- Выборка уникальных цветов глаз и волос
+SELECT DISTINCT eye, hair
+FROM MarvelCharacters
+
+-- Gold Eyes, Hazel Eyes, One Eye
+-- Yellow Hair, Red Hair, No Hair
+SELECT name, eye, hair, appearances
+FROM MarvelCharacters
+WHERE eye IN ('Gold Eyes', 'Hazel Eyes', 'One Eye')
+AND hair IN ('Yellow Hair', 'Red Hair', 'No Hair')
