b682b7644496420034fae46bd1dd53d8a770df22
Vladimir <vladimir.monin2016@yandex.ru>
2025-02-20 21:54:05 +0500

    lesson_35: первые запросы в SQL

---
+++ lesson_35.sql
@@ -0,0 +1,12 @@
+-- Lesson 35. Первые запросы к базе данных Marvel
+
+-- 1. ВЫБРАТЬ ВСЁ из таблицы MarvelCharacters
+SELECT *
+FROM MarvelCharacters;
+
+-- 2. Выбрать name, appearances из таблицы MarvelCharacters
+SELECT name, appearances
+FROM MarvelCharacters;
+
+-- ; - это разделитель запросов, чтобы можно было писать несколько запросов в одном файле
+
