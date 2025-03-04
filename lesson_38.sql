90f70fed8308b460f41b47b9b48996fc5c0ab214
Vladimir <vladimir.monin2016@yandex.ru>
2025-03-04 21:57:02 +0500

    lesson_38: WITH для связанных таблиц

--- lesson_38.sql
+++ lesson_38.sql
@@ -4,56 +4,115 @@
 -- Update
 
 
--- DDL MarvelCharacters
+-- DDL Базы Marvel Normal
+
 -- CREATE TABLE MarvelCharacters (
 --     id               INTEGER PRIMARY KEY AUTOINCREMENT,
 --     page_id          INTEGER,
 --     name             TEXT,
 --     urlslug          TEXT,
---     identify         TEXT,
---     ALIGN            TEXT,
---     EYE              TEXT,
---     HAIR             TEXT,
---     SEX              TEXT,
---     GSM              TEXT,
---     ALIVE            TEXT,
+--     identity_id      INTEGER,
+--     align_id         INTEGER,
+--     eye_id           INTEGER,
+--     hair_id          INTEGER,
+--     sex_id           INTEGER,
+--     status_id        INTEGER,
 --     APPEARANCES      INTEGER,
 --     FIRST_APPEARANCE TEXT,
---     Year             INTEGER
+--     Year             INTEGER,
+--     FOREIGN KEY (
+--         identity_id
+--     )
+--     REFERENCES Identity (identity_id),
+--     FOREIGN KEY (
+--         align_id
+--     )
+--     REFERENCES Alignment (align_id),
+--     FOREIGN KEY (
+--         eye_id
+--     )
+--     REFERENCES EyeColor (eye_id),
+--     FOREIGN KEY (
+--         hair_id
+--     )
+--     REFERENCES HairColor (hair_id),
+--     FOREIGN KEY (
+--         sex_id
+--     )
+--     REFERENCES Sex (sex_id),
+--     FOREIGN KEY (
+--         status_id
+--     )
+--     REFERENCES LivingStatus (status_id) 
 -- );
 
+-- -- Посмотрим все цвета глаз
+-- SELECT * FROM EyeColor
 
--- Операция создания. Create
--- Оператор INSERT
+-- -- id 2 Blue Eyes
 
-INSERT INTO MarvelCharacters (name)
-VALUES ('Potato-Man');
+-- -- Вставка данных
+-- INSERT INTO MarvelCharacters
+-- (name, eye_id)
+-- VALUES ('CatMan', 2);
+
+-- -- Выборка данных
+-- SELECT * FROM MarvelCharacters
+-- WHERE name LIKE '%CatMan%';
+
+
+-- Как это сделать в один запрос?
+
+INSERT INTO MarvelCharacters
+(name, eye_id)
+VALUES (
+    "CatWoman",
+    (
+        SELECT eye_id
+        FROM EyeColor
+        WHERE color LIKE 'Blue%'
+    )
+)
 
 SELECT * FROM MarvelCharacters
-WHERE name LIKE '%Potato%';
+WHERE name LIKE 'Cat%';
 
--- Операция Update
--- Оператор UPDATE
+
+-- Проверим цвет глаз персонажа через JOIN
+SELECT mc.name, ec.color
+FROM MarvelCharacters AS mc
+JOIN EyeColor AS ec ON mc.eye_id = ec.eye_id
+WHERE name LIKE 'Cat%';
+
+-- У нас возник дубль CatWoman без указанного цвета глаз
+-- Давайте укажем ей Amber Eyes
 
 UPDATE MarvelCharacters
-SET year = 2025, hair = 'blond', alive = 'yes'
-WHERE name like '%Potato%';
+SET eye_id = (
+    SELECT eye_id
+    FROM EyeColor
+    WHERE color = "Amber Eyes"
+)
+WHERE name = "CatWoman" AND eye_id IS NULL
 
--- Сделаем выборку ID подзапросом, и у одного из них обновим данные
-UPDATE MarvelCharacters
-SET year = 2026, hair = 'bolt', alive = 'no', name = 'Potato-Woman'
-WHERE ID = (
-    SELECT ID
-    FROM MarvelCharacters
-    WHERE name LIKE '%Potato%'
-    LIMIT 1
-);
-
--- Операция Delete
--- Оператор DELETE
-
+-- Удалим созданных персонажей через IN
 DELETE FROM MarvelCharacters
-WHERE name LIKE '%Potato%';
+WHERE name IN ('CatMan', 'CatWoman')
 
-SELECT * FROM MarvelCharacters
-WHERE name LIKE '%Potato%';
+
+-- Сделам сводную таблицу через WITH 
+-- Чтобы там были персонажи со всеми данными а так же ID
+WITH MarvelCharactersFull AS (
+    SELECT mc.*, id.identity AS identity_name, al.name AS align_name, ec.color AS eye_color, hc.color AS hair_color, s.name AS sex_name, ls.status AS status_name
+    FROM MarvelCharacters AS mc
+    JOIN Identity AS id ON mc.identity_id = id.identity_id
+    JOIN Alignment AS al ON mc.align_id = al.align_id
+    JOIN EyeColor AS ec ON mc.eye_id = ec.eye_id
+    JOIN HairColor AS hc ON mc.hair_id = hc.hair_id
+    JOIN Sex AS s ON mc.sex_id = s.sex_id
+    JOIN LivingStatus AS ls ON mc.status_id = ls.status_id
+)
+SELECT * FROM MarvelCharactersFull
+
+
+-- Используем эту заготовку для создания персонажа с цветом Brown Eyes и White Hair
